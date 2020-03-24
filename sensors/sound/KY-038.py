8f6cfd3b660fcbbb126305b94193f0307ea7fc3c
mstankovic <mjashica@gmail.com>
2020-03-24 23:09:43 +0100

    added sound sensor example
    Signed-off-by: mstankovic <mjashica@gmail.com>

---
+++ sensors/sound/KY-038.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callbeck(channel):
+    if GPIO.input(channel):
+        print("Sound Detected!")
+    else:
+        print("Sound Detected!")
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callbeck)
+
+while True:
+    time.sleep(1)
